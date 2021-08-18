63c6a5fc48c5bcf72d3dee9dcf5e990c7cff0d48
shravan20 <mrshravankumarb@gmail.com>
2021-08-18 06:16:02 +0530

    :sparkles: Implementation of Singly LinkedList
    Implementation of Singly LinkedList

---
+++ SinglyLinkedList/LinkedList.java
@@ -0,0 +1,15 @@
+package SinglyLinkedList;
+
+class LinkedList {
+    Node head; // head of list
+
+    /*
+        LinkedList Node
+    */
+    class Node {
+        Integer data;
+        Node next;
+        Node(Integer data) { this.data = data; }
+    }
+
+}
